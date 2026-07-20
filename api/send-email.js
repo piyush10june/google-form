d3c56af373756fda5e82beb7704a80c2107cdbfe
Your GitHub Username <guriyajain.sunny@gmail.com>
2026-07-20 09:37:09 +0530

    added test email

---
+++ api/send-email.js
@@ -0,0 +1,45 @@
+const nodemailer = require("nodemailer");
+
+module.exports = async (req, res) => {
+
+    if (req.method !== "POST") {
+        return res.status(405).json({
+            message: "Method Not Allowed"
+        });
+    }
+
+    try {
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: process.env.EMAIL_USER,
+            subject: "Blueberry AstroVastu Form Submission",
+            text: "Test email from your website.",
+        };
+
+        await transporter.sendMail(mailOptions);
+
+        res.status(200).json({
+            success: true
+        });
+
+    } catch (err) {
+
+        console.error(err);
+
+        res.status(500).json({
+            success: false,
+            error: err.message
+        });
+
+    }
+
+};
